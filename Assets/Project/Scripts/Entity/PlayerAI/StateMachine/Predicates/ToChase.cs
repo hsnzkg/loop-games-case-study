c8f78df8680b6b59999ec6bf8597e036bf9054c4
hsnzkg <hzekigogebakan@gmail.com>
2026-02-09 12:44:06 +0300

    AI Updates

--- Assets/Project/Scripts/Entity/PlayerAI/StateMachine/Predicates/ToChase.cs
+++ Assets/Project/Scripts/Entity/PlayerAI/StateMachine/Predicates/ToChase.cs
@@ -1,7 +1,78 @@
-﻿namespace Project.Scripts.Entity.PlayerAI.StateMachine.Predicates
+﻿using System.Collections.Generic;
+using Project.Scripts.Entity.Player;
+using Project.Scripts.FiniteStateMachine.Runtime;
+using Project.Scripts.Spawning.Spawners;
+using Project.Scripts.Storage.Runtime;
+using Project.Scripts.Storage.Storages;
+using Project.Scripts.Utility;
+using UnityEngine;
+
+namespace Project.Scripts.Entity.PlayerAI.StateMachine.Predicates
 {
-    public class ToChase
+    public class ToChase : IPredicate
     {
-        
+        private readonly List<PlayerEntity> m_playersInRangeBuffer;
+        private PlayerAIEntity[] m_aiLookupBuffer;
+
+        private readonly AISpawner m_aiSpawner;
+        private readonly AIStateContext m_stateContext;
+
+        public ToChase(AIStateContext context)
+        {
+            m_playersInRangeBuffer = new List<PlayerEntity>();
+            m_stateContext = context;
+            m_aiSpawner = Storage<GameplayStorage>.GetInstance().AISpawner;
+        }
+
+        public bool Evaluate()
+        {
+            GetPlayersInRange();
+            bool isDanger = DetectWeakness();
+            return isDanger;
+        }
+
+        private void GetPlayersInRange()
+        {
+            m_aiLookupBuffer = m_aiSpawner.GetPlayers();
+            m_playersInRangeBuffer.Clear();
+
+            PlayerEntity player = Storage<GameplayStorage>.GetInstance().Player;
+
+            Vector2 entityPosition = m_stateContext.EntityTransform.position.ToVector2XY();
+            for (int i = 0; i < m_aiLookupBuffer.Length; i++)
+            {
+                if (m_aiLookupBuffer[i] == null) continue;
+                if (m_aiLookupBuffer[i].GetIsDead()) continue;
+                if (m_aiLookupBuffer[i] == m_stateContext.Entity) continue;
+                Vector2 aiPos = m_aiLookupBuffer[i].transform.position.ToVector2XY();
+                float distance = Vector2.Distance(entityPosition, aiPos);
+                if (distance <= m_stateContext.AISettings.ChaseVisionThreshold)
+                {
+                    m_playersInRangeBuffer.Add(m_aiLookupBuffer[i]);
+                }
+            }
+
+            float playerDistance = Vector2.Distance(entityPosition, player.transform.position.ToVector2XY());
+            if (playerDistance <= m_stateContext.AISettings.ChaseVisionThreshold)
+            {
+                m_playersInRangeBuffer.Add(player);
+            }
+        }
+
+        private bool DetectWeakness()
+        {
+            foreach (PlayerEntity player in m_playersInRangeBuffer)
+            {
+                int weaponCount = player.GetWeaponCount();
+                int entityWeaponCount = m_stateContext.Entity.GetWeaponCount();
+                bool weaponFlag = entityWeaponCount < weaponCount;
+                bool healthFlag = m_stateContext.Entity.GetPercentHealth() > player.GetPercentHealth();
+                if (weaponFlag || healthFlag)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
     }
 }
--- Assets/Project/Scripts/Entity/PlayerAI/StateMachine/States/Chase.cs
+++ Assets/Project/Scripts/Entity/PlayerAI/StateMachine/States/Chase.cs
@@ -1,9 +1,95 @@
-﻿namespace Project.Scripts.Entity.PlayerAI.StateMachine.States
+﻿using System.Collections.Generic;
+using Project.Scripts.Entity.Player;
+using Project.Scripts.Entity.PlayerAI.Movement;
+using Project.Scripts.Level;
+using Project.Scripts.Spawning.Spawners;
+using Project.Scripts.Storage.Runtime;
+using Project.Scripts.Storage.Storages;
+using Project.Scripts.Utility;
+using UnityEngine;
+
+namespace Project.Scripts.Entity.PlayerAI.StateMachine.States
 {
     public class Chase : AIStateBase
     {
+        private readonly List<PlayerEntity> m_playersInRangeBuffer;
+        private PlayerAIEntity[] m_aiLookupBuffer;
+        private readonly AISpawner m_aiSpawner;
+        private readonly LevelManager m_levelManager;
+        
         public Chase(AIStateContext context) : base(context)
         {
+            m_playersInRangeBuffer = new List<PlayerEntity>();
+            m_aiSpawner = Storage<GameplayStorage>.GetInstance().AISpawner;
+            m_levelManager = Storage<GameplayStorage>.GetInstance().LevelManager;
+        }
+
+        protected override void OnEnter()
+        {
+            SetChasePosition();
+        }
+
+        public override void Tick(float deltaTime)
+        {
+            SetChasePosition();
+        }
+
+        private void SetChasePosition()
+        {
+            m_aiLookupBuffer = m_aiSpawner.GetPlayers();
+            m_playersInRangeBuffer.Clear();
+            
+            PlayerEntity player = Storage<GameplayStorage>.GetInstance().Player;
+
+            Vector2 entityPosition = Context.EntityTransform.position.ToVector2XY();
+            for (int i = 0; i < m_aiLookupBuffer.Length; i++)
+            {
+                if (m_aiLookupBuffer[i] == null) continue;
+                if (m_aiLookupBuffer[i].GetIsDead()) continue;
+                if (m_aiLookupBuffer[i] == Context.Entity) continue;
+                Vector2 aiPos = m_aiLookupBuffer[i].transform.position.ToVector2XY();
+                float distance = Vector2.Distance(entityPosition, aiPos);
+                
+                if (distance <= Context.AISettings.ChaseVisionThreshold)
+                {
+                    m_playersInRangeBuffer.Add(m_aiLookupBuffer[i]);
+                }
+            }
+
+            float playerDistance = Vector2.Distance(entityPosition, player.transform.position.ToVector2XY());
+            if (playerDistance <= Context.AISettings.ChaseVisionThreshold)
+            {
+                m_playersInRangeBuffer.Add(player);
+            }
+            
+            PlayerEntity closestPlayer = null;
+            float closestDistance = float.MaxValue;
+            foreach (PlayerEntity p in m_playersInRangeBuffer)
+            {
+                Vector2 playerPos = p.transform.position.ToVector2XY();
+                float distance = Vector2.Distance(entityPosition, playerPos);
+                
+                if (distance <= Context.AISettings.ChaseVisionThreshold)
+                {
+                    if (distance < closestDistance)
+                    {
+                        closestDistance = distance;
+                        closestPlayer = p;
+                    }
+                }
+            }
+
+            if (closestPlayer != null)
+            {
+                Vector2 threatDir = (closestPlayer.transform.position.ToVector2XY() - entityPosition).normalized;
+                Vector2 escapeDir = threatDir;
+                
+                float escapeDistance = Context.AISettings.EscapeMoveLenght;
+                Vector2 desiredPos = entityPosition + escapeDir * escapeDistance;
+
+                Vector2 finalPos = m_levelManager.ClampInsideArea(desiredPos, 0.5f);
+                ((AIInputProvider)Context.AIInputProvider).SetPositionTarget(finalPos); 
+            }
         }
     }
 }
